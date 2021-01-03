5eeb9531b2db5cd7b69cdd3bb266b23aafff70fc
jshuntley <joshuaehuntley@gmail.com>
2021-01-03 09:58:15 -0700

    fixed install

--- install.sh
+++ install.sh
@@ -51,8 +51,8 @@ show_menu () {
 
            sudo mv ./config/splash.png /usr/share/plymouth/themes/pix/splash.png
 
-           sed "s@#DIR#@${PWD}@g" config/craftbeerpiboot > /etc/init.d/craftbeerpiboot
-           chmod 755 /etc/init.d/craftbeerpiboot;
+           sed "s|#DIR#|${PWD}|g" config/craftbeerpiboot > /etc/init.d/craftbeerpiboot
+           chmod +x /etc/init.d/craftbeerpiboot;
 
            whiptail --title "Installition Finished" --msgbox "CraftBeerPi installation finished! You must hit OK to continue." 8 78
            show_menu
@@ -70,8 +70,9 @@ show_menu () {
        3)
            confirmAnswer "Are you sure you want to add CraftBeerPi to autostart"
            if [ $? = 0 ]; then
-             sed "s@#DIR#@${PWD}@g" config/craftbeerpiboot > /etc/init.d/craftbeerpiboot
-             chmod 755 /etc/init.d/craftbeerpiboot;
+             sed 's/#DIR#/${PWD}/g' config/craftbeerpiboot > /etc/init.d/craftbeerpiboot
+            #  chmod 755 /etc/init.d/craftbeerpiboot;
+             chmod +x /etc/init.d/craftbeerpiboot;
              update-rc.d craftbeerpiboot defaults;
              whiptail --title "Added succesfull to autostart" --msgbox "The CraftBeerPi was added to autostart succesfully. You must hit OK to continue." 8 78
              show_menu
